1da56115e4565f5a7e23d0fbaf58aa8606e7a87c
IvanKozlov95 <ikozlov9000@gmail.com>
2016-03-29 20:06:41 +0300

    Add config

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var Server = require('./lib/server');
 var log = require('util/log')(module);
-require('shelljs/global');
-var server_port = process.env.PORT || 1337;
-var server = new Server(server_port);
+var config = require('config');
+require('lib/mongoose');
 
-if (exec('gulp').code == 0) {
-	server.run();
-}
+var server = new Server(config.get('port'));
+
+server.run();
