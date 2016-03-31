360a0f03663ef3ef477e77212153bc66d6180477
IvanKozlov95 <ikozlov9000@gmail.com>
2016-03-31 09:01:33 +0300

    Remove extra strings

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var express = require('express');
 var Server = require('./lib/server');
 var log = require('util/log')(module);
 var config = require('config');
-require('lib/mongoose');
 
 var server = new Server(config.get('port'));
 
