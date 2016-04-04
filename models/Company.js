3222479a49f931bde34ef4fea85f53359a883bc2
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-04 09:14:05 +0300

    Add methods for interaction with plans

--- models/Company.js
+++ models/Company.js
@@ -1,12 +1,43 @@
 var mongoose = require('../lib/mongoose'),
     Schema   = mongoose.Schema,
-    User     = mongoose.model('User');
-
+    User     = mongoose.model('User'),
+    Plan 	 = mongoose.model('Plan'),
+    log 	 = require('../util/log')(module),
+    async 	 = require('async');
+ 
 
 var CompanySchema = new Schema({
-  schemas: [],
+  plans: [ Schema.Types.ObjectId ],
   logo: String,
   address: String
 });
+
+CompanySchema.methods.createPlan = function(plan, cb) {
+	this.plans = this.plans && [];
+
+	plan = new Plan({
+		tables: plan.tables
+	});
+	plan.save(cb);
+
+	this.plans.push(plan.id);
+	this.save(function(err) {
+		if (err) cb(err);
+	});
+}
+
+CompanySchema.methods.getPlans = function(cb) { 
+	async.map(this.plans, function(planId, callback) {
+		Plan.findById(planId, function(err, plan) {
+			if (err) callback(err);
+
+			if (plan) {
+				callback(null, plan);
+			} else {
+				log.warn("Couldn't find plan with id: " + planId);
+			}
+		})
+	}, cb);
+}
 
 User.discriminator('Company', CompanySchema);
