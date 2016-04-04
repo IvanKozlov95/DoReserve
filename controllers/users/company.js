4f8ed6defef93e8ee55160914f36ade619b9cfae
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-04 09:13:01 +0300

    Add routes for plans(schemas)

--- controllers/users/company.js
+++ controllers/users/company.js
@@ -1,14 +1,86 @@
-var express = require('express');
-var router = express.Router();
-var mongoose = require('../../lib/mongoose');
+var express   = require('express'),
+	router 	  = express.Router(),
+	mw 	 	  = require('../../middleware/authMw'),
+	mongoose  = require('../../lib/mongoose'),
+	Plan 	  = mongoose.model('Plan'),
+	Company   = mongoose.model('Company'),
+	log 	  = require('../../util/log')(module);
 
-router.get('/', (req, res, next) => {
+// ************
+// Home routes
+// ************
+router.get('/', mw.mustCompany, (req, res, next) => {
 	res.render('company');
 });
 
-router.post('/', (req, res, next) => {
-	console.log(req.body);
-	res.status(200).send("OK");
+// ************
+// Plans routes
+// ************
+router.get('/plans', function(req, res, next) {
+	Company.findById(req.user.id, function(err, company) {
+		if (err) return next(err);
+
+		company.getPlans(function(err, plans) {
+			if (err) return next(err);
+
+			log.info('Plans: ' + plans);
+			res.render('planList', {
+				plans: plans
+			});
+		})
+	});
 });
+
+router.get('/plan/:id', function(req, res, next) {
+	log.warn('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+	Plan.findById(req.params.id).lean().exec(function(err, plan) {
+		if (err) return next(err);
+
+		if (plan) {
+			res.render('plan', {
+				plan: plan
+			});
+		} else {
+			res.status(404).end();
+		}
+	});
+});
+
+router.get('/plan', function(req, res, next) {
+	res.render('plan');
+});
+
+router.post('/plan', function(req, res, next) {
+	var data = req.body;
+
+	if (data.id) {
+		Plan.findById(data.id, function(err, plan) {
+			if (err) return next(err);
+
+			if (plan) {
+				plan.tables = data.tables;
+				plan.save(function(err) {	
+					if (err) throw err;
+
+					log.info("Plan've been updated");
+				});
+			} else {
+				log.info('Plan not found');
+			}
+		});
+	} else {
+		Company.findById(req.user.id, function(err, company) {
+			if (err) return next(err);
+
+			company.createPlan(data, function(err) {
+				if (err) return next(err);
+
+				log.info('Created a new plan');
+			});
+		})
+	}
+
+	res.status(200).send("OK");
+})
 
 module.exports = router;
