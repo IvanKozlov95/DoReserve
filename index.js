eb0593ccb55f224a25614550327d1cc9322711be
Ivan Kozlov (Akvelon) <v-ivkozl@microsoft.com>
2016-04-04 11:57:50 +0300

    Fix typo in module paths and packadge deps

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Server = require('./lib/server');
-var log = require('util/log')(module);
-var config = require('config');
+var log = require('./util/log')(module);
+var config = require('./config');
 
 var server = new Server(config.get('port'));
 
