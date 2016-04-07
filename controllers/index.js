1c178f467ed28db2166435ef523cb63815f4b6a4
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-08 00:24:50 +0300

    Added new controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
-exports.Company  = require('./users/company.js');
-exports.User 	 = require('./users/user.js');
+exports.Company  = require('./users/company');
+exports.User 	 = require('./users/user');
+exports.Plan	 = require('./models/plan');
+exports.Table	 = require('./models/table');
 exports.login	 = require('./login');
 exports.register = require('./register');
 exports.logout 	 = require('./logout');
