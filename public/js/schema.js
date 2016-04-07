7a6b56c099a18bf3e680bd1c172be2eacc2aa73c
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-08 00:24:08 +0300

    Fixed json export and table ids

--- public/js/schema.js
+++ public/js/schema.js
@@ -1,14 +1,12 @@
 (function() {
-  var Schema, btnExport, btnSave, iconsPath, schema, tableSize,
-    bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  var Schema, btnExport, btnSave, iconsPath, schema, tableSize;
 
   iconsPath = 'icons.svg';
 
   tableSize = 20;
 
   Schema = (function() {
-    function Schema() {
-      this.toJSON = bind(this.toJSON, this);
+    function Schema(owner, data) {
       this.parent = $('.schema-container')[0];
       this.paper = Snap(this.parent.children[0]);
       this.paper.attr({
@@ -16,6 +14,8 @@
         height: 500
       });
       this.tables = this.paper.g();
+      this.owner = owner;
+      this.fromJSON(data);
       this._bindEvents();
     }
 
@@ -27,16 +27,26 @@
       return this.addTable(e.offsetX, e.offsetY, tableSize);
     };
 
-    Schema.prototype.addTable = function(x, y, r) {
+    Schema.prototype.addTable = function(x, y, r, id) {
       var table;
-      table = this.paper.circle(x, y, r).addClass('schema-table');
+      table = this.paper.circle(x, y, r).addClass('schema-table').attr('id', id).click((function(_this) {
+        return function(e) {
+          _this._tableInfo(e.target.id);
+          return e.stopPropagation();
+        };
+      })(this));
       return this.tables.add(table);
+    };
+
+    Schema.prototype._tableInfo = function(tableId) {
+      return window.location = '/table?p=' + this.id + '&t=' + tableId;
     };
 
     Schema.prototype.toJSON = function() {
       var attr, i, len, query, ref, res, table, x;
       res = {
-        id: this.parent.id,
+        company: this.owner,
+        id: this.id,
         tables: []
       };
       x = Snap.parse(schema.paper.innerSVG());
@@ -46,25 +56,29 @@
         table = ref[i];
         attr = table.node.attributes;
         res.tables.push({
+          id: attr.id.value,
           x: attr.cx.value,
           y: attr.cy.value,
           r: attr.r.value
         });
       }
+      console.log(res);
       return res;
     };
 
     Schema.prototype.fromJSON = function(data) {
       var i, len, ref, results, table;
-      $(this.parent).attr('id', data._id);
-      if (data.tables) {
-        ref = data.tables;
-        results = [];
-        for (i = 0, len = ref.length; i < len; i++) {
-          table = ref[i];
-          results.push(this.addTable(table.x, table.y, table.r));
+      if (data) {
+        this.id = data._id;
+        if (data.tables) {
+          ref = data.tables;
+          results = [];
+          for (i = 0, len = ref.length; i < len; i++) {
+            table = ref[i];
+            results.push(this.addTable(table.x, table.y, table.r, table._id));
+          }
+          return results;
         }
-        return results;
       }
     };
 
@@ -72,12 +86,10 @@
 
   })();
 
-  schema = new Schema;
+  schema = {};
 
-  window.loadPlan = function(data) {
-    if (data != null) {
-      return schema.fromJSON(data);
-    }
+  window.createSchema = function(owner, data) {
+    return schema = new Schema(owner, data);
   };
 
   btnSave = $('.btn-save-schema')[0];
@@ -86,16 +98,18 @@
 
   btnSave.onclick = function(event) {
     return $.ajax({
-      url: '/company/plan',
+      url: '/plan',
       method: 'POST',
       dataType: 'json',
-      data: schema.toJSON(),
+      data: {
+        plan: schema.toJSON()
+      },
       statusCode: {
         404: function() {
           return alert('Not Found');
         },
         200: function() {
-          return alert('Saved');
+          return window.location.reload();
         }
       }
     });
