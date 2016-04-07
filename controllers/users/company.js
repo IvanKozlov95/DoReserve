c3c0bf25ecee3c096f0e079171b64abd3abb0784
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-08 00:17:44 +0300

    Removed plan's functionality

--- controllers/users/company.js
+++ controllers/users/company.js
@@ -23,64 +23,12 @@ router.get('/plans', function(req, res, next) {
 		company.getPlans(function(err, plans) {
 			if (err) return next(err);
 
-			log.info('Plans: ' + plans);
+			log.info(plans.toString());
 			res.render('planList', {
 				plans: plans
 			});
 		})
 	});
 });
-
-router.get('/plan/:id', function(req, res, next) {
-	log.warn('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-	Plan.findById(req.params.id).lean().exec(function(err, plan) {
-		if (err) return next(err);
-
-		if (plan) {
-			res.render('plan', {
-				plan: plan
-			});
-		} else {
-			res.status(404).end();
-		}
-	});
-});
-
-router.get('/plan', function(req, res, next) {
-	res.render('plan');
-});
-
-router.post('/plan', function(req, res, next) {
-	var data = req.body;
-
-	if (data.id) {
-		Plan.findById(data.id, function(err, plan) {
-			if (err) return next(err);
-
-			if (plan) {
-				plan.tables = data.tables;
-				plan.save(function(err) {	
-					if (err) throw err;
-
-					log.info("Plan've been updated");
-				});
-			} else {
-				log.info('Plan not found');
-			}
-		});
-	} else {
-		Company.findById(req.user.id, function(err, company) {
-			if (err) return next(err);
-
-			company.createPlan(data, function(err) {
-				if (err) return next(err);
-
-				log.info('Created a new plan');
-			});
-		})
-	}
-
-	res.status(200).send("OK");
-})
 
 module.exports = router;
