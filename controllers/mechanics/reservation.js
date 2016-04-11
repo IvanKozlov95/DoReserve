d3ce787397f32dc7af1d1c9a4242b7ce025e170d
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-11 09:17:05 +0300

    Add reservation controller

---
+++ controllers/mechanics/reservation.js
@@ -0,0 +1,75 @@
+var express     = require('express'),
+	router 	    = express.Router(),
+	mw 	 	    = require('../../middleware/authMw'),
+	mongoose    = require('../../lib/mongoose'),
+	ObjectId    = mongoose.Types.ObjectId;
+	Reservation = mongoose.model('Reservation'),
+	Plan 	    = mongoose.model('Plan'),
+	log 	    = require('../../util/log')(module);
+
+router.get('/:id', function(req, res, next) {
+	// cast to id 
+	try {
+		var id = new ObjectId(req.params.id)
+	} catch (e) { }
+
+	Reservation.findById(id, function(err, reserve) {
+		if (err) return next(err);
+
+		if (reserve) {
+			if (reserve.client == req.user.id) {
+				res.render('reservation', {
+					reservation: reserve
+				});
+			} else {
+				res.status(403).end();
+			}
+		} else {
+			res.status(404).end();
+		}
+	})
+});
+
+router.post('/create', function(req, res, next) {
+	var planId = req.body.plan,
+		tableId = req.body.table,
+		date = req.body.date,
+		time = {
+			from: req.body['start-time'],
+			to: req.body['end-time']
+		},
+		client = req.user.id,
+		persons = req.body.persons;
+
+
+	Plan.findById(planId, function(err, plan) {
+		if (err) return next(err);
+
+		if (plan) {
+			plan.findTable(tableId, function(err, table) {
+				if (err) return next(err);
+
+				if (table) {
+					Reservation.create({
+						plan: planId,
+						table: tableId,
+						date: date,
+						time: time,
+						client: client,
+						persons: persons
+					}, function(err, reserve) {
+						if (err) return next(err);
+
+						req.user.addReservation(reserve.id, function(err) {
+							if (err) return next(err);
+						});
+
+						res.redirect('/user/home');
+					});
+				}
+			});
+		}
+	});
+});
+
+module.exports = router;
