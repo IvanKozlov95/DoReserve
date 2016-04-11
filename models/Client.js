632f0e5d6fcbcdb2635b8143f4f329cf767e819b
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-11 09:14:08 +0300

    Added getReservations method

--- models/Client.js
+++ models/Client.js
@@ -1,13 +1,29 @@
-var mongoose = require('../lib/mongoose'),
-    Schema   = mongoose.Schema,
-    User     = mongoose.model('User');
-
+var mongoose 	= require('../lib/mongoose'),
+    Schema   	= mongoose.Schema,
+    User     	= mongoose.model('User'),
+	Reservation = mongoose.model('Reservation'),
+    async 	 	= require('async');
 
 var ClientSchema = new Schema({
   friendlyName: String,
-  reservedTables: [],
+  reservations: [ Schema.Types.ObjectId ],
   favoriteCompanies: [],
   friends: []
 });
+
+ClientSchema.methods.getReservations = function(filter, cb) {
+	//todo: add filter
+	log.info(this.reservations);
+	async.map(this.reservations, function(item, callback) {
+    Reservation.findById(item, callback);
+  }, cb);
+}
+
+ClientSchema.methods.addReservation = function(reserve, cb) {
+  if (reserve) {
+    this.reservations.push(reserve);
+    this.save(cb);
+  }
+}
 
 User.discriminator('Client', ClientSchema);
