1a1707378ca9b81d4d3d4c1aeaea530c0e344b25
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-12 23:29:30 +0300

    Space instead of tabs

--- controllers/login.js
+++ controllers/login.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	passport.authenticate('local',
+  passport.authenticate('local',
     function(err, user, info) {
       if (err) return next(err);
       if (user) {
