69b98ffd73edc90b3d3b739bc24d10e4ea9aa7f7
IvanKozlov95 <ikozlov9000@gmail.com>
2016-04-15 00:19:31 +0300

    Changes in reCaptcha mw

--- middleware/reCaptcha.js
+++ middleware/reCaptcha.js
@@ -1,17 +1,12 @@
 var request = require('request');
 
 module.exports = function(req, res, next) {
-	if (!req.body['g-recaptcha-response']) {
-		return res.status(400).send('Please check the the captcha form.');
-	}
-
 	request.post({
 		url: 'https://www.google.com/recaptcha/api/siteverify?secret=6LfxJx0TAAAAACdfiQBt166V59GM_1yxQNTWBlhv&response='+ req.body['g-recaptcha-response']
 	}, function(err, httpResponse, body) {
 		var success = JSON.parse(body).success;
-
 		if (success) return next();
 
-		return res.status(403).send('You shall not pass spamer!');
+		return res.status(403).send('Please reenter the captcha');
 	});
 }
