27a1d7f862c9868f523cfd920bfe0205046e1ff6
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-03 22:53:38 +0300

    Add user's profile

--- controllers/users/user.js
+++ controllers/users/user.js
@@ -1,9 +1,53 @@
 var express   = require('express'),
 	router 	  = express.Router(),
-	auth 	  = require('../../middleware/authMw'); 
+	auth 	  = require('../../middleware/authMw'),
+	mongoose  = require('../../lib/mongoose'),
+	log 	  = require('../../util/log')(module);
 
 router.get('/home', auth.mustAuthenticated, function(req, res, next) {
 	res.redirect('/' + req.user.__t + '/home');
 });
+
+router.get('/profile', auth.mustAuthenticated, function(req, res, next) {
+	var edit = !!req.query.edit;
+	var User = mongoose.model('User');
+
+	User
+		.findById(req.user.id)
+		// .lean()
+		.exec((err, user) => {
+			if (err) return next(err);
+			log.info(user.toJSON());
+			res.render('user/profile', {
+				user: user,
+				json: user.toJSON && user.toJSON(),
+				edit: edit
+			});
+		})
+});
+
+router.post('/update', auth.mustAuthenticated, function(req, res, next) {
+	if (req.user.id != req.body.id) {
+		return res.status(403).end();
+	}
+
+	User.findById(req.body.id, (err, user) => {
+		if (err) return next(err);
+
+		if (user) {
+			for (var i in req.body) {
+				user[i] = req.body[i];
+			}
+			user.save((err) => {
+				if (err) return next(err);
+
+				log.info('User was updated');
+				res.redirect('/user/profile');
+			});
+		} else {
+			res.status(404).end();
+		}
+	});
+})
 
 module.exports = router;
