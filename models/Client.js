7147b88195c0af1ed0a31cfd13f4287c7de70102
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-05 16:19:59 +0300

    Added client create anon method

--- models/Client.js
+++ models/Client.js
@@ -1,13 +1,25 @@
 var mongoose 	= require('../lib/mongoose'),
     Schema   	= mongoose.Schema,
     User     	= mongoose.model('User'),
-    async 	 	= require('async');
+    async 	 	= require('async'),
+    crypto   = require('crypto');
 
 var ClientSchema = new Schema({
   reservations: [ Schema.Types.ObjectId ],
   favoriteCompanies: [],
   friends: []
 });
+
+ClientSchema.statics.createAnon = function(options, cb) {
+  var client = new this({
+    username: crypto.createHmac('sha1', Math.random() + '').update(Date.now).digest('hex'),
+    password: Math.random() + '',
+    name: 'Anonymous',
+    email: options.email
+  });
+
+  client.save(cb);
+}
 
 ClientSchema.methods.getReservations = function(filter, cb) {
 	//todo: add filter
