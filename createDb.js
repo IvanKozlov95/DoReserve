d207ebe78694a2bf451a0cffbf74b946be0c7eef
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-22 16:32:06 +0300

    Fixed issue: didn't create second reservation

--- createDb.js
+++ createDb.js
@@ -14,15 +14,15 @@ function dropDatabase(cb) {
 function requireModels(cb) {
 	require('models');
 
-	async.each(Object.keys[mongoose.models], function(modelName, cb) {
-		mongoose.models[modelName].ensureIndexes(cb);
-	}, cb);
+    async.each(Object.keys[mongoose.models], function(modelName, cb) {
+		mongoose.models[modelName].ensureIndexes(cb);     
+	}, cb); 
 }
 
 function createUsers(cb) {
 	var users = [
 		new mongoose.models.Client({username: 'Ivan', password: '123', name: 'Ivan Kozlov', email: 'ikozlov9000@gmail.com', phone: '7 (915) 812 56 69'}),
-		new mongoose.models.Client({username: 'Vasya', password: '123'}),
+		new mongoose.models.Client({username: 'Vasya', email: 'Vasya@Vasya.Vasya', password: '123'}),
 		new mongoose.models.Client({username: 'admin', password: '123'}),
 		new mongoose.models.Company({username: 'company1', password: '123', name: 'Rocco', email: 'asd@asd.com', phone: '92-90-05', address: 'Pushkina sq., 2', desc: 'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy.'}),
 		new mongoose.models.Company({username: 'company2', password: '123', name: 'Company', email: 'company@company.com', phone: '123-456-789', address: 'Schorsa st., 68', desc: 'One advanced diverted domestic sex repeated bringing you old. Possible procured her trifling laughter thoughts property she met way. Companions shy had solicitude favourable own. Which could saw guest man now heard but. Lasted my coming uneasy marked so should. Gravity letters it amongst herself dearest an windows by. Wooded ladies she basket season age her uneasy saw. Discourse unwilling am no described dejection incommode no listening of. Before nature his parish boy.'})
@@ -34,8 +34,19 @@ function createUsers(cb) {
 }
 
 function createReservations(cb) {
-	createSingleReservation('Ivan', 'company1', cb);
-	createSingleReservation('Vasya', 'company1', cb);
+	var list = [
+		{
+			client: 'Ivan',
+			company: 'company1'
+		}, 
+		{
+			client: 'Vasya',
+			company: 'company1'
+		}
+	];
+	async.each(list, function(item, callback) {
+		createSingleReservation(item.client, item.company, callback);
+	}, cb)
 }
 
 
