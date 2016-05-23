ec4efded72f14a5a702b09f60c86c5028195484c
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-23 20:46:08 +0300

    Fixed issue: Captcha always responsedwith success = false

--- middleware/reCaptcha.js
+++ middleware/reCaptcha.js
@@ -1,12 +1,13 @@
-var request = require('request');
+var request   = require('request'),
+	HtmlError = require('../lib/HtmlError');
 
 module.exports = function(req, res, next) {
 	request.post({
-		url: 'https://www.google.com/recaptcha/api/siteverify?secret=6LfxJx0TAAAAACdfiQBt166V59GM_1yxQNTWBlhv&response='+ req.body['g-recaptcha-response']
+		url: 'https://www.google.com/recaptcha/api/siteverify?secret=6LfxJx0TAAAAACdfiQBt166V59GM_1yxQNTWBlhv&response=' + req.body['g-recaptcha-response']
 	}, function(err, httpResponse, body) {
 		var success = JSON.parse(body).success;
 		if (success) return next();
 
-		return res.status(403).send('Please reenter the captcha');
+		return next(new HtmlError('Please reenter the captcha', 403));
 	});
 }
