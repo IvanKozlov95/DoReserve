d92368af01226a673c40c498d2c200a7d4350675
IvanKozlov95 <ikozlov9000@gmail.com>
2016-05-25 18:43:32 +0300

    Add retriveClientInfo method

--- util/common.js
+++ util/common.js
@@ -3,7 +3,7 @@ var mongoose = require('../lib/mongoose');
 /**
  * Checks if emails exists in database
  * @email - email to check
- * @returns True if email do not exists and false if do.
+ * @returns Promise that returns true if email do not exists and false if do.
  */
 function checkEmailUnique(email) {
 	var User = mongoose.model('User');
@@ -16,4 +16,45 @@ function checkEmailUnique(email) {
 	});
 }
 
-module.exports.checkEmail = checkEmailUnique;
+/**
+ * Retrieve client information
+ * @id - client's id
+ * @returns Promise that returns client information.
+ */
+ function retrieveClientInfoById(id, withReservation) {
+	var Client = mongoose.model('Client');
+
+	return new Promise((resolve, reject) => {
+		Client
+			.findById(id)
+			.exec(function(err, client) {
+				if (err) reject(err);
+
+				if (client) {
+					if (withReservation) {
+						client.getReservations(null, function(err, reservations) {
+							if (err) reject(err);
+							
+							resolve({
+								client: client.toJSON(),
+								reservations: reservations
+							});
+						});
+					} else {
+						resolve({
+							client: client.toJSON()
+						});
+					}
+				} else {
+					resolve(null);
+				}
+			});
+	});
+}
+
+
+
+module.exports = {
+	checkEmail: checkEmailUnique,
+	retrieveClientInfoById: retrieveClientInfoById
+}
